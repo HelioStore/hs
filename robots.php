19b58849ae3ad18e5129a8f0bee26a399bfb6e4d
George Provotorov <admin@heliostore.ru>
2016-06-01 11:52:27 +0300

    Update robots.php

--- robots.php
+++ robots.php
@@ -3,23 +3,19 @@ header('Content-Type: text/plain; charset=utf-8');
 if($_SERVER['HTTP_HOST']=='heliostore.ru'){
 echo 'User-agent: *
 Disallow: /cart/*
-Disallow: /compare/*
-Disallow: /reservation/*
-Disallow: /auth/*
 Disallow: /register/*
-Disallow: /my/*
-Disallow: /oneclick/*
+Disallow: /etc/*
 Host: https://heliostore.ru/
 Sitemap: https://heliostore.ru/sitemap-1.xml';
-} else if($_SERVER['HTTP_HOST']=='heliora.ru'){
+} else if($_SERVER['HTTP_HOST']=='2nd-domain.com'){
 echo 'User-agent: *
 Disallow: /
-Host: https://heliostore.ru/';
+Host: https://2nd-domain.com/';
 } 
-else if($_SERVER['HTTP_HOST']=='geliostore.ru'){
+else if($_SERVER['HTTP_HOST']=='3rd-domain.com'){
 echo 'User-agent: *
 Disallow: /
-Host: https://heliostore.ru/';
+Host: https://3rd-domain.com/';
 } 
 else {echo 'User-agent: *
 Disallow: /';}
